c25efcca7fd5c4459bb08af0a66400e3c81bcfea
Ripley Marvin <ripleyem@hotmail.com>
2018-01-30 11:27:59 -0800

    Tweaked login pages for Devise notices, customized user settings page, set up passing data for additional user fields

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,16 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
   def confirm_login
     if !user_signed_in?
       redirect_to new_user_session_path
     end
+  end
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:account_update, keys: [:user_name, :full_name, :company])
   end
 end
