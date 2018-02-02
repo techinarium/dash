019facae3570820c55f2e4b1f58236cb14794395
Morgan <morgan_joseph@hotmail.com>
2018-02-02 15:53:12 -0800

    Heroku mailer stuff

--- config/environment.rb
+++ config/environment.rb
@@ -4,12 +4,13 @@ require_relative 'application'
 # Initialize the Rails application.
 Rails.application.initialize!
 
+ActionMailer::Base.delivery_method = :smtp
 ActionMailer::Base.smtp_settings = {
-  :user_name => 'app86441478@heroku.com',
-  :password => '',
-  :domain => 'techinarium-dash.com',
-  :address => 'smtp.sendgrid.net',
-  :port => 587,
-  :authentication => :plain,
-  :enable_starttls_auto => true
+  address:              'smtp.sendgrid.net',
+  port:                 587,
+  domain:               'heroku.com',
+  user_name:            ENV["SENDGRID_USERNAME"],
+  password:             ENV["SENDGRID_PASSWORD"],
+  authentication:       'plain',
+  enable_starttls_auto: true
 }
