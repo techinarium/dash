8b6cf7b7f8d4060d62ceafd218a4b9b49f73ae33
Katelynn West <katelynnwest18@gmail.com>
2018-02-08 18:08:33 -0800

    Finished styling dashboard and widget list.

--- app/assets/javascripts/menu.js
+++ app/assets/javascripts/menu.js
@@ -1,18 +1,39 @@
 document.addEventListener('DOMContentLoaded', function(){
-    //document.getElementById("menuBtn").hidden = true;
-    //document.getElementsByClassName("menu-link").hidden = true;
-    $(".menu-link").hide();
+    $("#installed-widgets").hide();
+    $(".menu-link").hide(); //hide the Account Settings, Logout & Widget Store links
 
     $("#menuBtn").click(function(e){
         e.preventDefault();
-
-        //show the menu links (Account Settings, Widget Store, Logout)
-        $(".menu-link").fadeToggle();
-
         if(this.innerHTML === "MENU"){
+            //change text to "close", then show links & widget list
             this.innerHTML = "CLOSE";
+            $(".menu-link").fadeToggle();
+            $("#widget-list").css("visibility", "visible").fadeIn();
+            $("main").css("display", "inline-block");
         } else if (this.innerHTML === "CLOSE"){
+            //change link back to "MENU", hide links and widget list
             this.innerHTML = "MENU";
+            $(".menu-link").fadeToggle();
+            $("#widget-list").css("display", "none");
+            $("main").css("display", "block");
         }
+    });
+
+    //when "My Widgets" link is clicked, show My Widgets & hide Installed Widgets
+    $("#my-widgets-tab").click(function(e){
+        e.preventDefault();
+        $("#installed-widgets-tab").toggleClass("active-tab");
+        $("#my-widgets-tab").toggleClass("active-tab");
+        $("#installed-widgets").hide();
+        $("#my-widgets").fadeIn();
+    });
+
+    //when "Installed" link is clicked, show Installed widgets & hide My Widgets
+    $("#installed-widgets-tab").click(function(e){
+        e.preventDefault();
+        $("#my-widgets-tab").toggleClass("active-tab");
+        $("#installed-widgets-tab").toggleClass("active-tab");
+        $("#my-widgets").hide();
+        $("#installed-widgets").fadeIn();
     });
 }, false);
