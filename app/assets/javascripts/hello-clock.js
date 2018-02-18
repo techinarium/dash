2e9c394272e47838fda6e313724ba168bb986d76
Tony McCoy <tony@ratwizard.io>
2018-02-18 01:16:39 -0800

    Dashboard stylesheet tweaks and JS auto-update clock

---
+++ app/assets/javascripts/hello-clock.js
@@ -0,0 +1,36 @@
+$(() => {
+  const $clock = $("#hello-clock")
+  
+  function zeroPad(value) {
+    let str = value.toString()
+    
+    while (str.length < 2) {
+      str = '0' + str
+    }
+    
+    return str
+  }
+  
+  function updateTime() {
+    const date = new Date()
+    
+    let m = 'AM'
+    let hours = date.getHours()
+    let minutes = date.getMinutes()
+    // let seconds = date.getSeconds()
+    
+    if (hours > 12) {
+      hours -= 12
+      m = 'PM'
+    }
+    
+    const time = `${hours}:${zeroPad(minutes)} ${m}`
+    console.log(time)
+    
+    $clock.text(time)
+  }
+  
+  setInterval(updateTime, 10 * 1000)
+  
+  updateTime()
+})
